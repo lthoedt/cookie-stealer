63715ab62fb30fd8c49fdd35c62a75433fe9915c
lthoedt <lthoedt@gmail.com>
2023-02-12 14:22:19 +0100

    print cookies

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,18 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+var cookies = require("cookie-parser");
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+app.use(cookies());
+
 app.get("/", (req: Request, res: Response) => {
 	console.log(req.query);
-	console.log(req.cookies);
+	console.log(JSON.stringify(req.cookies));
+	console.log(JSON.stringify(req.signedCookies));
   res.send("Express + TypeScript Server");
 });
 
